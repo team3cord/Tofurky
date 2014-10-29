9d5f042025f7098967a931357c37b3738c8fd177
MattCordeiro <matthew3cord@gmail.com>
2014-10-28 20:51:24 -0700

    Update search/ responsive header/marquee to ipad

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form action="/" method="get">
+<form action="<?php echo bloginfo('url'); ?>" method="get">
     <fieldset class="green-text" >
-        <label  for="search">Search in <?php echo home_url(); ?></label>
+        <label  for="search">Search in <?php echo bloginfo('url'); ?></label>
         <input type="text" name="s" id="search"placeholder="What are you looking for?" value="<?php the_search_query(); ?>" />
         <input type="image" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/imgs/search-open.jpg" />
     </fieldset>
