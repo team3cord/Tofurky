c5e59da256a1b538b4f9ca3d9372d4b8696dab37
Matthew Cordeiro <matthew3cord@gmail.com>
2015-12-16 21:29:25 -0800

    Fixed permalink

--- content-masonry.php
+++ content-masonry.php
@@ -7,11 +7,11 @@
             $cat_name = $cats[0]-> name; ?>
             <div class="masonry-thumbnail">
                <!-- <a href="<?//php echo esc_url( home_url( '/' ) ); ?>category/<?//php  echo $cat_name ?>"><div class="post-cat-icon <?//php  echo $cat_name ?>"></div></a>-->
-                <a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('masonry-thumb'); ?></a>
+                <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('masonry-thumb'); ?></a>
             </div><!--.masonry-thumbnail-->
         <?php endif; ?>
         <div class="masonry-details">
-            <h5><a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><span class="masonry-post-title"> <?php the_title(); ?></span></a></h5>
+            <h5><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><span class="masonry-post-title"> <?php the_title(); ?></span></a></h5>
             <div class="com-tag">
                 <span class="comment-num"><?php echo comments_number(); ?></span>
                 <span class="tag-list"><?php echo get_the_tag_list(' ',', ',' '); ?></span>
